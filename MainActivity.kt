e0987d5fdd54c4e2eabaa34b5b4fec48b9657365
minseok1015 <107205708+minseok1015@users.noreply.github.com>
2024-05-20 20:25:29 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.rollingpaper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.rollingpaper.homePage.homeScreen
+import com.example.rollingpaper.ui.theme.RollingPaperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RollingPaperTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    homeScreen();
+                }
+            }
+        }
+    }
+}
+
